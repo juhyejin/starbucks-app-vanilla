32015f63e9ff973a5c4c2c9caa5ec880eb7e55b5
juhyejin <juhyejin.dev@gmail.com>
2023-03-02 16:52:32 +0900

    스타벅스 페이지 예제 완료

--- js/main.js
+++ js/main.js
@@ -15,6 +15,12 @@ searchInputEl.addEventListener('blur', function(){
 
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
+toTopEl.addEventListener('click', function(){
+    gsap.to(window, .7, {
+        scrollTo: 0
+    });
+})
 
 window.addEventListener('scroll', _.throttle(function(){
     if( window.scrollY > 500 ){
@@ -24,11 +30,19 @@ window.addEventListener('scroll', _.throttle(function(){
             opacity: 0,
             display: 'none',
         });
+        //버튼보이기
+        gsap.to(toTopEl, .2, {
+            x: 0,
+        });
     }else{
         //배지 보이기
         gsap.to(badgeEl, .6, {
             opacity: 1,
             display: 'block'
+        });
+        //버튼 숨기기
+        gsap.to(toTopEl, .2, {
+            x: 100,
         });
     }
 },300));
@@ -54,9 +68,9 @@ new Swiper('.promotion .swiper-container',{
     spaceBetween: 10, //슬라이드 사이 여백
     centeredSlides: true, //1번 슬라이드가 가운데 보이기
     loop: true,
-    // autoplay: {
-    //     delay: 5000
-    // }
+    autoplay: {
+        delay: 5000
+    },
     pagination: {
         el: '.promotion .swiper-pagination',// 페이지 번호 요소 선택자
         clickable: true,
@@ -65,18 +79,65 @@ new Swiper('.promotion .swiper-container',{
         prevEl: '.promotion .swiper-prev',
         nextEl: '.promotion .swiper-next'
     }
-})
+});
+
+new Swiper('.awards .swiper-container',{
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    navigation: {
+        prevEl: '.awards .swiper-prev',
+        nextEl: '.awards .swiper-next'
+    }
+});
 
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
 let isHidePromotion = false;
 promotionToggleBtn.addEventListener('click',function(){
-    isHidePromotion = !isHidePromotion
+    isHidePromotion = !isHidePromotion;
     if (isHidePromotion){
         //숨김 처리
         promotionEl.classList.add('hide');
     }else{
         // 보임 처리
-        promotionEl.classList.remove('hide')
+        promotionEl.classList.remove('hide');
     }
-})
+});
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+    // `.toFixed()`를 통해 반환된 문자 데이터를,
+    // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector,delay, size){
+    gsap.to(selector, random(1.5, 2.5), {
+        y: size,
+        repeat: -1,
+        yoyo: true,
+        ease: Power1.easeInOut,
+        delay: random(0, delay)
+    });
+}
+
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+    new ScrollMagic
+      .Scene({
+          triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+          triggerHook: .8, //
+      })
+      .setClassToggle(spyEl, 'show')
+      .addTo(new ScrollMagic.Controller());
+});
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
