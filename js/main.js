640dc53f6a6352ae8c438677f3dff275909c3f1e
juhyejin <juhyejin.dev@gmail.com>
2023-03-02 11:58:42 +0900

    욕소슬라이드 설정

--- js/main.js
+++ js/main.js
@@ -17,7 +17,6 @@ searchInputEl.addEventListener('blur', function(){
 const badgeEl = document.querySelector('header .badges');
 
 window.addEventListener('scroll', _.throttle(function(){
-    console.log(window.scrollY);
     if( window.scrollY > 500 ){
         // 배지 숨기기
         //gsap.to(요소, 지속시간, 옵션);
@@ -49,3 +48,35 @@ new Swiper('.notice-line .swiper-container',{
     autoplay : true,
     loop: true,
 });
+
+new Swiper('.promotion .swiper-container',{
+    slidesPerView : 3, //한번에 보여줄 슬라이드 개수
+    spaceBetween: 10, //슬라이드 사이 여백
+    centeredSlides: true, //1번 슬라이드가 가운데 보이기
+    loop: true,
+    // autoplay: {
+    //     delay: 5000
+    // }
+    pagination: {
+        el: '.promotion .swiper-pagination',// 페이지 번호 요소 선택자
+        clickable: true,
+    },
+    navigation:{
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+})
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click',function(){
+    isHidePromotion = !isHidePromotion
+    if (isHidePromotion){
+        //숨김 처리
+        promotionEl.classList.add('hide');
+    }else{
+        // 보임 처리
+        promotionEl.classList.remove('hide')
+    }
+})
